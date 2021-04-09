80b7823d86dc06674a940527907046bbfd90d3d4
lewisseo91 <togokr@naver.com>
2021-04-09 12:39:10 +0900

    feat : about, interview 작성 css 제외

--- burette/src/pages/Home.js
+++ burette/src/pages/Home.js
@@ -3,79 +3,88 @@ import "./Home.scss";
 const Home = () => {
     return (
         <div className={`home`}>
-            <section className={`left-container home-section`}>
-                {/* 인터뷰 */}
-                <section className={`home-grid`}>
-                    <div className={`home-grid-title`}>
-                        <h2 className={`home-title`}>
-                            Interview
-                        </h2>
-                    </div>
+            <section className={`home-grid-total`}>
+                <section className={`left-container home-section`}>
+                    {/* 인터뷰 */}
+                    <section className={`home-grid`}>
+                        <div className={`home-grid-title`}>
+                            <h2 className={`home-title`}>
+                                Interview
+                            </h2>
+                        </div>
+                    </section>
+                    {/* 시리즈 저널 */}
+                    <section className={`home-grid`}>
+                        <div className={`home-grid-title`}>
+                            <h2 className={`home-title`}>
+                                series journal
+                            </h2>
+                        </div>
+                    </section>
                 </section>
-                {/* 시리즈 저널 */}
-                <section className={`home-grid`}>
-                    <div className={`home-grid-title`}>
-                        <h2 className={`home-title`}>
-                            series journal
-                        </h2>
-                    </div>
+                <section className={`mid-container home-section`}>
+                    {/* 브랜드 밋업 */}
+                    <section className={`home-grid`}>
+                        <div className={`home-grid-title`}>
+                            <h2 className={`home-title`}>
+                                Brand Meet Up
+                            </h2>
+                        </div>
+                    </section>
+                    {/* C Question */}
+                    <section className={`home-grid`}>
+                        <div className={`home-grid-title`}>
+                            <h2 className={`home-title`}>
+                                C. Question
+                            </h2>
+                        </div>
+                    </section>
+                    {/* G Oversea */}
+                    <section className={`home-grid`}>
+                        <div className={`home-grid-title`}>
+                            <h2 className={`home-title`}>
+                                G. Oversea
+                            </h2>
+                        </div>
+                    </section>
+                </section>
+                <section className={`right-container home-section`}>
+                    {/* Column */}
+                    <section className={`home-grid`}>
+                        <div className={`home-grid-title`}>
+                            <h2 className={`home-title`}>
+                                Column
+                            </h2>
+                        </div>
+                    </section>
+                    {/* Info Desk */}
+                    <section className={`home-grid`}>
+                        <div className={`home-grid-title`}>
+                            <h2 className={`home-title`}>
+                                Info Desk
+                            </h2>
+                        </div>
+                    </section>
+                    {/* After Letter */}
+                    <section className={`home-grid`}>
+                        <div className={`home-grid-title`}>
+                            <h2 className={`home-title`}>
+                                After Letter
+                            </h2>
+                        </div>
+                        {/* 3 row  */}
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </section>
                 </section>
             </section>
-            <section className={`mid-container home-section`}>
-                {/* 브랜드 밋업 */}
-                <section className={`home-grid`}>
-                    <div className={`home-grid-title`}>
-                        <h2 className={`home-title`}>
-                            Brand Meet Up
-                        </h2>
-                    </div>
-                </section>
-                {/* C Question */}
-                <section className={`home-grid`}>
-                    <div className={`home-grid-title`}>
-                        <h2 className={`home-title`}>
-                            C. Question
-                        </h2>
-                    </div>
-                </section>
-                {/* G Oversea */}
-                <section className={`home-grid`}>
-                    <div className={`home-grid-title`}>
-                        <h2 className={`home-title`}>
-                            G. Oversea
-                        </h2>
-                    </div>
-                </section>
-            </section>
-            <section className={`right-container home-section`}>
-                {/* Column */}
-                <section className={`home-grid`}>
-                    <div className={`home-grid-title`}>
-                        <h2 className={`home-title`}>
-                            Column
-                        </h2>
-                    </div>
-                </section>
-                {/* Info Desk */}
-                <section className={`home-grid`}>
-                    <div className={`home-grid-title`}>
-                        <h2 className={`home-title`}>
-                            Info Desk
-                        </h2>
-                    </div>
-                </section>
-                {/* After Letter */}
-                <section className={`home-grid`}>
-                    <div className={`home-grid-title`}>
-                        <h2 className={`home-title`}>
-                            After Letter
-                        </h2>
-                    </div>
-                    {/* 3 row  */}
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                </section>
+            <section className={`home-news-subscribe`}>
+                <div>
+                    <h2>
+                        Subscribe to the burette NewsLetter ()
+                    </h2>
+                </div>
             </section>
         </div>
     );
--- burette/src/shared/App.js
+++ burette/src/shared/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
-import { Home, About, Posts } from '../pages';
+import { Home, About, Posts, Interview } from '../pages';
 
 class App extends Component {
     render() {
@@ -12,6 +12,7 @@ class App extends Component {
                     <Route path="/about" component={About}/>
                 </Switch>
                 <Route path="/posts" component={Posts}/>
+                <Route path="/interview/:interviewId" component={Interview}/>
             </div>
         );
     }
