197f2f1924b444f9e50b5c4c27602ed0cbc17a58
lewisseo91 <togokr@naver.com>
2021-04-17 16:19:41 +0900

    style : 디렉토리 구조 정리

--- burette/src/pages/post/Posts.js
+++ burette/src/pages/post/Posts.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link, Route } from 'react-router-dom';
-import { Post } from '../pages'; 
+import { Post } from '../index';
 
 const Posts = ({match}) => {
     return (
