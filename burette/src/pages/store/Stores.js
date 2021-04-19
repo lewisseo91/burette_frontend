09d9399670534309d6a1ab3dab2aec2aa5930e69
lewisseo91 <togokr@naver.com>
2021-04-19 16:53:13 +0900

    feat : store grid 추가

--- burette/src/pages/store/Stores.js
+++ burette/src/pages/store/Stores.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import queryString from 'query-string';
+import './Stores.scss';
 
 const Stores = ({location, match}) => {
 
@@ -7,33 +8,93 @@ const Stores = ({location, match}) => {
         <section className={`store`}>
             <section className={`store-grid`}>
                 <section className={`store-grid-content`}>
-                    <div>
-                        [ *메인 썸네일 이미지* ]
+                    <div className={`store-grid-content-header`}>
+                        <div className={`store-grid-content-subtitle`}>
+                            <span className={`store-grid-content-subtitle-txt`}>비건푸드 및 비건레시피</span>
+                        </div>
+                        <div className={`store-grid-content-title`}>
+                            <span className={`store-grid-content-title-txt`}>비건푸드도 맛있게 시작하세요.</span>
+                        </div>
+                    </div>
+                    <div className={`store-grid-content-thumbnail`}>
+                        <div className={`store-grid-content-thumbnail-img`}>
+                            <img className={`res-img`} src="/images/ex4.jpg" alt="ex4"/>
+                        </div>
                     </div>
                 </section>
                 <section className={`store-grid-content`}>
-                    <div>
-                        [ *메인 썸네일 이미지* ]
+                    <div className={`store-grid-content-header`}>
+                        <div className={`store-grid-content-subtitle`}>
+                            <span className={`store-grid-content-subtitle-txt`}>비건푸드 및 비건레시피</span>
+                        </div>
+                        <div className={`store-grid-content-title`}>
+                            <span className={`store-grid-content-title-txt`}>비건푸드도 맛있게 시작하세요.</span>
+                        </div>
+                    </div>
+                    <div className={`store-grid-content-thumbnail`}>
+                        <div className={`store-grid-content-thumbnail-img`}>
+                            <img className={`res-img`} src="/images/ex4.jpg" alt="ex4"/>
+                        </div>
                     </div>
                 </section>
                 <section className={`store-grid-content`}>
-                    <div>
-                        [ *메인 썸네일 이미지* ]
+                    <div className={`store-grid-content-header`}>
+                        <div className={`store-grid-content-subtitle`}>
+                            <span className={`store-grid-content-subtitle-txt`}>비건푸드 및 비건레시피</span>
+                        </div>
+                        <div className={`store-grid-content-title`}>
+                            <span className={`store-grid-content-title-txt`}>비건푸드도 맛있게 시작하세요.</span>
+                        </div>
+                    </div>
+                    <div className={`store-grid-content-thumbnail`}>
+                        <div className={`store-grid-content-thumbnail-img`}>
+                            <img className={`res-img`} src="/images/ex4.jpg" alt="ex4"/>
+                        </div>
                     </div>
                 </section>
                 <section className={`store-grid-content`}>
-                    <div>
-                        [ *메인 썸네일 이미지* ]
+                    <div className={`store-grid-content-header`}>
+                        <div className={`store-grid-content-subtitle`}>
+                            <span className={`store-grid-content-subtitle-txt`}>비건푸드 및 비건레시피</span>
+                        </div>
+                        <div className={`store-grid-content-title`}>
+                            <span className={`store-grid-content-title-txt`}>비건푸드도 맛있게 시작하세요.</span>
+                        </div>
+                    </div>
+                    <div className={`store-grid-content-thumbnail`}>
+                        <div className={`store-grid-content-thumbnail-img`}>
+                            <img className={`res-img`} src="/images/ex4.jpg" alt="ex4"/>
+                        </div>
                     </div>
                 </section>
                 <section className={`store-grid-content`}>
-                    <div>
-                        [ *메인 썸네일 이미지* ]
+                    <div className={`store-grid-content-header`}>
+                        <div className={`store-grid-content-subtitle`}>
+                            <span className={`store-grid-content-subtitle-txt`}>비건푸드 및 비건레시피</span>
+                        </div>
+                        <div className={`store-grid-content-title`}>
+                            <span className={`store-grid-content-title-txt`}>비건푸드도 맛있게 시작하세요.</span>
+                        </div>
+                    </div>
+                    <div className={`store-grid-content-thumbnail`}>
+                        <div className={`store-grid-content-thumbnail-img`}>
+                            <img className={`res-img`} src="/images/ex4.jpg" alt="ex4"/>
+                        </div>
                     </div>
                 </section>
                 <section className={`store-grid-content`}>
-                    <div>
-                        [ *메인 썸네일 이미지* ]
+                    <div className={`store-grid-content-header`}>
+                        <div className={`store-grid-content-subtitle`}>
+                            <span className={`store-grid-content-subtitle-txt`}>비건푸드 및 비건레시피</span>
+                        </div>
+                        <div className={`store-grid-content-title`}>
+                            <span className={`store-grid-content-title-txt`}>비건푸드도 맛있게 시작하세요.</span>
+                        </div>
+                    </div>
+                    <div className={`store-grid-content-thumbnail`}>
+                        <div className={`store-grid-content-thumbnail-img`}>
+                            <img className={`res-img`} src="/images/ex4.jpg" alt="ex4"/>
+                        </div>
                     </div>
                 </section>
             </section>
