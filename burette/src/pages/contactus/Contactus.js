c7e1f83652100e7bafcc3cbf5812b4b4d63753b2
lewisseo91 <togokr@naver.com>
2021-04-19 17:23:16 +0900

    feat : contactus 스타일 추가 완료, 1차 페이지 스타일 추가 완료

--- burette/src/pages/contactus/Contactus.js
+++ burette/src/pages/contactus/Contactus.js
@@ -1,40 +1,51 @@
 import React from 'react';
 import queryString from 'query-string';
+import './Contactus.scss';
 
 const Contactus = ({location, match}) => {
 
     return (
-        <section className={`Contactus`}>
-            <section className={`Contactus-title`}>
+        <section className={`contactus`}>
+            <section className={`contactus-title`}>
                 Contact us,
             </section>
-            <section className={`Contactus-content`}>
-                <form action="/post">
-                    <div>
-                        <label htmlFor="">이름</label>
-                        <input type="text"/>
-                    </div>
-                    <div>
-                        <label htmlFor="">연락처</label>
-                        <input type="text"/>
-                    </div>
-                    <div>
-                        <label htmlFor="">이메일</label>
-                        <input type="text"/>
-                    </div>
-                    <div>
-                        <label>상담내용</label>
-                        <textarea name="" id="" cols="30" rows="10"></textarea>
-                    </div>
-                    <button type="submit">제출</button>
-                </form>
-            </section>
-            <section className={`Contactus-footer`}>
-                <div>
-                    CALL NUMBER. 070-7677-0928
+            <form className={`contactus-content`} action="/post">
+                <div className={`contactus-content-element`}>
+                    <label className={`contactus-content-element-label`} htmlFor="">이름</label>
+                    <input className={`contactus-content-element-inp`} type="text"/>
                 </div>
-                <div>
-                    CALL TIME. 09:30~18:00
+                <div className={`contactus-content-element`}>
+                    <label className={`contactus-content-element-label`} htmlFor="">연락처</label>
+                    <input className={`contactus-content-element-inp`} type="text"/>
+                </div>
+                <div className={`contactus-content-element`}>
+                    <label className={`contactus-content-element-label`} htmlFor="">이메일</label>
+                    <input className={`contactus-content-element-inp`} type="text"/>
+                </div>
+                <div className={`contactus-content-element`}>
+                    <label className={`contactus-content-element-label-txtarea`}>상담내용</label>
+                    <textarea className={`contactus-content-element-txtarea`} name="" id="" cols="30" rows="10"></textarea>
+                </div>
+                <div className={`contactus-content-element form-element-submit`}>
+                    <button className={`contactus-content-element-submit-btn cursor-pointer`} type="submit">제출</button>
+                </div>
+            </form>
+            <section className={`contactus-footer`}>
+                <div className={`contactus-footer-element`}>
+                    <div className={`contactus-footer-element-title`}>
+                        <span>CALL NUMBER.</span>
+                    </div>
+                    <div className={`contactus-footer-element-content`}>
+                        <span>070-7677-0928</span>
+                    </div>
+                </div>
+                <div className={`contactus-footer-element`}>
+                    <div className={`contactus-footer-element-title`}>
+                        <span>CALL TIME.</span>
+                    </div>
+                    <div className={`contactus-footer-element-content`}>
+                        <span>09:30~18:00</span>
+                    </div>
                 </div>
             </section>
         </section>
